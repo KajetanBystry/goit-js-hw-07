36f2a3eac973290436b8ec3c96f113fad210da31
KajetanBystry <kajetanbystry@gmail.com>
2023-12-20 19:37:19 +0100

    ostatnimamnadzieje

---
+++ goit-js-hw-07/js/task-04.js
@@ -0,0 +1,35 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+let number = 0 
+const boxDiv = document.querySelector("#boxes")
+const buttons = document.querySelectorAll("button");
+document.querySelector("input").addEventListener("input", value => {
+  number = value.target.value;
+});
+buttons[0].addEventListener("click", createBoxes)
+buttons[1].addEventListener("click", destroyBoxes)
+
+function createBoxes(amount){
+  let divHeight = 30
+  amount = number
+  console.log(amount)
+  for (let i = 0; i < amount; i++){
+    boxDiv.insertAdjacentHTML(
+    "beforeend",
+    `<div style="width: 30px; height: ${divHeight}px; background-color: ${getRandomHexColor()}"></div>`
+    )
+  divHeight += 10
+  }
+}
+
+function destroyBoxes(){
+  boxDiv.innerHTML = ""
+}
+
+
+
+
